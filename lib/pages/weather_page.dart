5940daa01588007711eebd6a83427ef073b85366
Kiwanuka <kiwanukagodfrey21@gmail.com>
2025-03-25 20:27:06 +0300

    (init: Create weather application repository) Created the weather appplication project and added the scarfold widget as a skelton for the application.

---
+++ lib/pages/weather_page.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Weatherpage extends StatefulWidget {
+  const Weatherpage({super.key});
+
+  @override
+  State<Weatherpage> createState() => _WeatherpageState();
+}
+
+class _WeatherpageState extends State<Weatherpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    );
+  }
+}
+
+
+ 
